0205df3c710a4ce7bcf396fc543f2b2788b8c0c8
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-02-21 11:55:57 -0400

    installation of packages to run backend 2023

--- index.ts
+++ index.ts
@@ -10,31 +10,30 @@ import reporteRoutes from './routes/reportes';
 
 const server = new Server();
 
-
 // Body parser
-server.app.use(bodyParser.urlencoded({ extended: true}));
-server.app.use(bodyParser.json() );
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
 
 // configuración del CORDS
-server.app.use(cors({ origin: true, credentials:true}));
-
+server.app.use(cors({origin: true, credentials: true}));
 
 // Rutas de mi aplicación
-server.app.use( '/user', userRoutes );
-server.app.use( '/reportes', reporteRoutes );
-
+server.app.use('/user', userRoutes);
+server.app.use('/reportes', reporteRoutes);
 
 // Conectar Base de Datos
 // Cuando no es creada la BD directamente de aquí la crea automáticamente
-mongoose.connect('mongodb://localhost:27017/cantvAverias',
-     { useNewUrlParser: true, useCreateIndex: true}, (err)=>{
+mongoose.connect(
+	'mongodb://localhost:27017/cantvAverias',
+	{useNewUrlParser: true, useCreateIndex: true},
+	(err) => {
+		if (err) throw err;
 
-      if (err) throw err;
-
-      console.log('Base de Datos ONLINE') 
-     })
+		console.log('Base de Datos ONLINE');
+	}
+);
 
 // Levantando express
-server.start(()=>{
- console.log(`Servidor corriendo en el puerto ${server.port}`);
+server.start(() => {
+	console.log(`Servidor corriendo en el puerto ${server.port}`);
 });
